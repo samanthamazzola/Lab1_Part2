c8d73af9e0d3832db4146fc8c3a7adb80461ceb1
samantha <smazzola7@gmail.com>
2018-06-20 02:06:44 -0400

    convert string to int and int to array

--- GCPrework_Lab1/Program.cs
+++ GCPrework_Lab1/Program.cs
@@ -1,79 +1,81 @@
 ﻿using System;
 
-namespace GCPrework_Lab1
+namespace GCPreworkLabPartOne
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int numSum1, numSum2;
-            Console.WriteLine("\n\n");
-            Console.WriteLine("Add each integer of two different numbers. Are the values the same?");
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine("Type a number with 3 integers:");
-            numSum1 = Convert.ToInt16(Console.ReadLine());
+            //variables
+            string number1, number2, sum;
+            char doAgain;
+            bool repeat1 = true, repeat2 = true;
 
-            Console.WriteLine("Type a different number with 3 integers:");
-            numSum2 = Convert.ToInt16(Console.ReadLine());
+            while (repeat1 == true)
+            {
+                //string input
+                Console.WriteLine("Please enter two numbers with the same amount of digits: ");
+                Console.WriteLine("Please enter your first number: ");
+                var number1 = Console.ReadLine();
+                Console.WriteLine("Please enter your second number: ");
+                var number2 = Console.ReadLine();
 
-            if (numSum1 == numSum2)
-                Console.WriteLine("{0} and {1} are equal.\n", numSum1, numSum2);
-            else 
-                Console.WriteLine("{0} and {1} are NOT equal.\n", numSum1, numSum2);
+                //convert and assign an int value from string input 
+                //OR int.Parse(number1); ????
+                var num1 = Convert.ToInt16(Console.ReadLine());
+                var num2 = Convert.ToInt16(Console.ReadLine());
+
+                for (int i = 1, i < num1, i++)
+                    if(num1[i] + num2[i] != num1[0] + num2[0])
+                        return = false;
+                {
+                    Console.WriteLine("False");
+                }
+                else if(num1[i] + num2[i] != num1[0] + num2[0])
+                    return = true;
+                {
+                    Console.WriteLine("True");
+                }
+
+
+
+                number1 = String.Concat(number1, "", number2);
+
+                while (repeat2 == true)
+                {
+                    Console.WriteLine("Total of " + number1 + ", is your value.");
+                    sum = Console.ReadLine();
+
+                    if (sum == "yes" || sum == "Yes" || sum == "YES")
+                    //parameters that will choose whether or not you move on to other statements
+                    {
+                        Console.WriteLine("Great news" + number1 " number.");
+                        repeat2 = false; // if any are a yes variation it allows the program to move on
+                    }
+                    else if (sum == "no" || sum == "No" || sum == "NO")
+                    {
+                        Console.WriteLine("Sorry " + number1 + " .");
+                        repeat2 = false;
+                    }
+                    else
+                    //always returns true
+                    {
+                        Console.WriteLine("Sorry, please input a new number: ");
+
+                    }
+                }
+                Console.WriteLine("Would you like to try this again? (Y or N)");
+                doAgain = Convert.ToChar(Console.ReadLine());
+                //since Read and Readline return string you need to convert 
+                if(doAgain == 'y' || doAgain == 'Y')
+                {
+                    repeat1 = true;
+                }
+                else
+                {
+                    repeat1 = false; 
+                }
+            }
         }
     }
 }
-
-
-
-
-
-
-
-            /*int number1 = 1;
-            if (number1 == 1) ;
-            Console.WriteLine(number1);
-
-            int number1, number2, numberSum;
-            char doAgain;
-            bool decision1 = true; decision2 = true;
-
-            Console.WriteLine("Please enter a value in hundreds.");
-            number1 = Console.ReadLine();
-
-            Console.WriteLine("Please enter another number in hundreds");
-            number2 = Console.ReadLine();
-
-
-
-            if(numberSum == "yes" || numberSum == "Yes" || numberSum == "YES")
-            {
-                Console.WriteLine(""
-            }
-
-
-
-
-
-
-            int number1, number2, solution = 1, total;
-            char doAgain;
-            bool decision = true;
-
-            while (decision == true)
-            {
-                solution = 1;
-                Console.WriteLine("Please enter a number with three digits");
-                number1 = Convert.ToInt16(Console.ReadLine());
-
-                Console.WriteLine("Please enter another number with three digits");
-                number2 = Convert.ToInt16(Console.ReadLine());
-
-                total
-
-                for (int temp = number1; temp > total; temp --)
-                {
-                    solution *= temp;
-                }
-            }*/
